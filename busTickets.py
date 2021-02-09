cdea5bd225120fe10a5a49b34cb583bd8e1a7854
mikelghedina <mikel.ghedina@gmail.com>
2021-02-09 10:16:59 +0100

    Initial commit

---
+++ busTickets.py
@@ -0,0 +1,27 @@
+class Tickets:
+    def __init__(self, sold, rest, total):
+        self.sold = sold
+        self.rest = rest
+        self.total = total
+
+    def getSold(self):
+        return self.sold
+
+    def setSold(self, sold):
+        self.sold = sold
+
+    def getRest(self):
+        return self.rest
+
+    def setRest(self, rest):
+        self.rest = rest
+
+    def getTotal(self):
+        return self.total
+
+    def setTotal(self, total):
+        self.total = total
+
+
+
+
