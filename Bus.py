cfb5c9c85fadfd2bef51b5c4fb330c6beee69808
mikelghedina <mikel.ghedina@gmail.com>
2021-02-09 14:00:37 +0100

    init

--- Bus.py
+++ Bus.py
@@ -1,37 +1,23 @@
 from busTickets import Ticket
 
-
 class Bus:
-    """ Clase Autobus """
-
     def __init__(self, id):
         self.id = id
         self.arrTickets = []
 
-    """ STATIC METHODS """
-
-    @staticmethod
-    def generarAutobuses(cantAutobuses):
-        arrAutobuses = []
-
-        for i in cantAutobuses:
-            bus = Bus(i)
-            arrAutobuses.append(bus)
-            print(bus)
-
-        return arrAutobuses
-
-    """GETTERS AND SETTERS"""
-
     def getIdBus(self):
         return self.id
+
+    def setIdBus(self, id):
+        self.id = id
 
     def setArrTickets(self, arrTickets):
         self.arrTickets = arrTickets
 
-    def __str__(self):
-        return str(self.id) + self.getArrTickets()
-
     def getArrTickets(self):
         for i in self.arrTickets:
             return i
+
+    def __str__(self):
+        return str(self.id) + self.getArrTickets()
+
